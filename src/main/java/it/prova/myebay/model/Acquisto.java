63fa3ecaef227ced6ee4df0bbaabdb3ffed3f238
Balraj1108 <balrajsingh1108@gmail.com>
2022-11-19 09:00:52 +0100

    Creazione model acquisto, annuncio, categoria

---
+++ src/main/java/it/prova/myebay/model/Acquisto.java
@@ -0,0 +1,17 @@
+package it.prova.myebay.model;
+
+import java.util.Date;
+
+public class Acquisto {
+
+	private Long id;
+	
+	private String descrizione;
+	
+	private Date data;
+	
+	private Integer prezzo;
+	
+	
+	private Utente utenteAcquirente;
+}
---
+++ src/main/java/it/prova/myebay/model/Annuncio.java
@@ -0,0 +1,20 @@
+package it.prova.myebay.model;
+
+import java.util.Date;
+
+public class Annuncio {
+
+	private Long id;
+	
+	private String testoAnnuncio;
+	
+	private Integer prezzo;
+	
+	private Date data;
+	
+	private Boolean aperto;
+	
+	private Utente utenteInserimento;
+	
+	
+}
